091466ff66ba8379e1a3323585994652a2f26b3b
zhuxp <nimezhu@gmail.com>
2018-01-15 16:11:44 -0500

    cls add rowGroup

--- CmdCls.go
+++ CmdCls.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/nimezhu/fire"
 	"github.com/urfave/cli"
@@ -16,5 +17,16 @@ func CmdCls(cli *cli.Context) {
 	log.Println(colGroup)
 	log.Println(rowGroup)
 	newMat := Table{colIds, rowIds, c, r, mat.RawMatrix().Data, t.FileName + "_cls", t.Name + "_cls"}
-	fmt.Print(newMat.PrettyString(-1))
+	s := newMat.PrettyString(-1) //TODO
+	lines := strings.Split(s, "\n")
+	header := strings.Split(lines[0], "\t")
+	fmt.Printf("%s\t%s\t%s\n", header[0], "rowGroup", strings.Join(header[1:], "\t"))
+	for i, line := range lines[1:] {
+		f := strings.Split(line, "\t")
+		if len(line) == 0 {
+			continue
+		}
+		fmt.Printf("%s\t%d\t%s\n", f[0], rowGroup[i], strings.Join(f[1:], "\t"))
+	}
+	//TODO
 }
